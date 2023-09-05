8b9b0b7a88b07066f0c8fd05ee87da42e45cc960
Ambuj-Pandey <78492933+Ambuj-Pandey@users.noreply.github.com>
2023-09-05 06:49:52 +0000

    Updated Renaming Logic

--- HMAC/src/Components/Student/Student.jsx
+++ HMAC/src/Components/Student/Student.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Navbar from "../Navbar/Navbar";
 import "./Student.css";
 import PDF from "../../assets/PDF.png";
@@ -6,10 +6,21 @@ import PDF from "../../assets/PDF.png";
 const Student = () => {
   const [selectedFile, setSelectedFile] = useState("");
   const [desc, setDesc] = useState("");
+  const renameInputRef = useRef(null);
+  const [isValid, setIsValid] = useState(true);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     setSelectedFile(file.name);
+  };
+
+  const handleRenameInputFocus = () => {
+    const inputElement = renameInputRef.current;
+    const indexOfExtension = selectedFile.lastIndexOf(".pdf");
+    
+    if (indexOfExtension > 0) {
+      inputElement.setSelectionRange(0, indexOfExtension);
+    }
   };
 
   return (
@@ -33,12 +44,20 @@ const Student = () => {
         </div>
         <div className="metaData">
           <label htmlFor="rename">Rename</label>
-          <input
-            type="text"
-            id="rename"
-            value={selectedFile} 
-            onChange={(e) => setSelectedFile(e.target.value)}
-          />
+            <input
+              type="text"
+              id="rename"
+              value={selectedFile}
+              onChange={(e) => 
+                {setSelectedFile(e.target.value);
+                  setIsValid(e.target.value.endsWith(".pdf"));}}
+              onFocus={handleRenameInputFocus} 
+              ref={renameInputRef} 
+              className={!isValid ? "invalid-input" : ""}
+            />
+          {!isValid && (
+            <div className="error-message">File name must end with '.pdf'</div>
+          )}
           <label htmlFor="description">Description</label>
           <textarea
             id="description"
