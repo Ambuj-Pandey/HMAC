a8c22c8454655ae55898f7924b395bd3345f3ee0
siddhN1811 <siddhanthgn@yahoo.com>
2023-09-07 22:29:17 +0530

    summary table

---
+++ HMAC/src/Components/StudentRecord/StudentRecord.jsx
@@ -0,0 +1,20 @@
+import "./StudentRecord.css"
+
+const StudentRecord = ()=>{
+
+    return(
+
+        <div className="student-column-container">
+           <div>2</div>
+           <div>2</div>
+           <div>2</div>
+           <div>2</div>
+           <div>2</div>
+
+
+
+        </div>
+    );
+}
+
+export default StudentRecord;
--- HMAC/src/Pages/SubmissionSummary/SubmissionSummary.jsx
+++ HMAC/src/Pages/SubmissionSummary/SubmissionSummary.jsx
@@ -1,9 +1,19 @@
 import "./SumbissionSummary.css";
 import Navbar from "../../Components/Navbar/Navbar";
+import StudentRecord from "../../Components/StudentRecord/StudentRecord";
 const SubmissionSummary = () => {
   return (
     <div className="submission-container">
-        <Navbar goTo="Student" ></Navbar>
+      <Navbar goTo="Student"></Navbar>
+      <div className="submission-bg-container">
+        <div className="submission-column-header">
+          <li>Roll No.</li>
+          <li>Student Name</li>
+          <li>AI content %</li>
+          <li>Duplicate content %</li>
+        </div>
+        <StudentRecord />
+      </div>
     </div>
   );
 };
