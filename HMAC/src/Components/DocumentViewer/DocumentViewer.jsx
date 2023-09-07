5aef6174c4c6e616b0db1409a381f39f20a8c25b
siddhN1811 <siddhanthgn@yahoo.com>
2023-09-07 13:09:24 +0530

    pdf toolbar fix

--- HMAC/src/Components/DocumentViewer/DocumentViewer.jsx
+++ HMAC/src/Components/DocumentViewer/DocumentViewer.jsx
@@ -5,7 +5,7 @@ const DocumentViewer = ({ pdfUrl }) => {
   return (
     <div className="document-container">
       <div className="document-frame">
-        <iframe src={pdfUrl} title="Document Viewer" />
+        <iframe src={pdfUrl+"#toolbar=0&navpanes=0"} title="Document Viewer" />
       </div>
     </div>
   );
