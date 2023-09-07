deb25e119e519367b9a67e002f1efbed0184dfea
Ambuj-Pandey <78492933+Ambuj-Pandey@users.noreply.github.com>
2023-09-07 20:45:50 +0530

    Fixes file preview in admin panel

--- API/base/urls.py
+++ API/base/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.getRoutes, name='routes'),
@@ -9,4 +11,4 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('Upload/', views.upload_file, name='Upload'),
     # path('logout/', views.LogoutView.as_view(), name ='logout')
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
