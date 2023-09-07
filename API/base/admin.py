660e62dd1b2c0be69e9862e2ac18b3b2e9262741
Ambuj-Pandey <78492933+Ambuj-Pandey@users.noreply.github.com>
2023-09-07 16:07:00 +0530

    Added FileUpload view in admin

--- API/base/admin.py
+++ API/base/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from base.models import User
+from base.models import FileModel
 
 admin.site.register(User)
+admin.site.register(FileModel)
 # Register your models here.
