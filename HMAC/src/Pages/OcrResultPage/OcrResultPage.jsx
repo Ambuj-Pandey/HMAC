3fb045e575904059cb6711b8490545dffdb2e551
Hrishikesh-Panigrahi <hrishikesh.panigrahi38@gmail.com>
2024-04-05 15:40:55 +0530

    something

--- HMAC/src/Pages/OcrResultPage/OcrResultPage.jsx
+++ HMAC/src/Pages/OcrResultPage/OcrResultPage.jsx
@@ -6,6 +6,8 @@ import axios from "axios";
 const OcrResultPage = () => {
   const { id } = useParams();
   const [ocrData, setOcrData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const fetchOcrResult = async () => {
       try {
@@ -16,6 +18,7 @@ const OcrResultPage = () => {
 
         if (response.status === 200) {
           setOcrData(response.data);
+          setLoading(false);
         } else {
           console.error("Error fetching data:", response.statusText);
         }
@@ -28,19 +31,27 @@ const OcrResultPage = () => {
   }, [id]);
 
   return (
-    
     <div className="OcrContainer">
-    {ocrData && (
-      <div>
-        <h1 className="OcrHeader">Ocr Result for ID: {ocrData.id}</h1>
-        <div>
-          <h3 className="OcrSubheader">Uploaded By: {ocrData.uploaded_by}</h3>
-          <p className="OcrResults">OCR Results: {ocrData.ocr_results}</p>
+      {loading ? (
+        <div className="loading">
+          <div className="spinner"></div>
         </div>
-      </div>
-    )}
-  </div>
-      
+      ) : (
+        <>
+          {ocrData && (
+            <div>
+              <h1 className="OcrHeader">Ocr Result for ID: {ocrData.id}</h1>
+              <div>
+                <h3 className="OcrSubheader">
+                  Uploaded By: {ocrData.uploaded_by}
+                </h3>
+                <p className="OcrResults">OCR Results: {ocrData.ocr_results}</p>
+              </div>
+            </div>
+          )}
+        </>
+      )}
+    </div>
   );
 };
 
